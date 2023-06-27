0ec72250f8a56bdcab3ca62bf9681dab68f255b1
Bruno Carter <112636008+brunodev12@users.noreply.github.com>
2023-06-27 02:09:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -64,8 +64,6 @@ for i,v in enumerate(collection_list):
         else:
             num_assets = 0
     for z in range(number_offers):
-        print(z)
-        print(v)
         if (num_assets > 0) and (num_assets > len(token_id_list)-z):
             trait = v['trait'] == 'yes'
             if trait:
