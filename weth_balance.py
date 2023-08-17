b340506043aa35dd259643fb9263a10e8e7320c6
brunodev12 <bruno.carthery12@gmail.com>
2023-08-16 23:15:44 -0300

    feat: get balance for all networks

--- weth_balance.py
+++ weth_balance.py
@@ -1,17 +1,31 @@
 from web3 import Web3
 import os
 
-ethereum_endpoint = os.environ.get('ETHEREUM_RPC')
-contract_address_weth = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-web3 = Web3(Web3.HTTPProvider(ethereum_endpoint))
+endpoints = {'ethereum': os.environ.get('ETHEREUM_RPC'),
+             'matic': os.environ.get('POLYGON_RPC')}
+
+contract_address = {'ethereum': '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+                    'matic': '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619'}
+
 
 def getWETHbalance(address):
-    address = web3.to_checksum_address(address)
-    
+
     contract_abi = (
         '[{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
-        )
+    )
 
-    contract = web3.eth.contract(web3.to_checksum_address(contract_address_weth), abi=contract_abi)
-    weth_balance = contract.functions.balanceOf(address).call()
-    return weth_balance/1E18
+    weth_balance = {}
+
+    for key, value in contract_address.items():
+        web3 = Web3(Web3.HTTPProvider(endpoints[key]))
+        address = web3.to_checksum_address(address)
+
+        contract = web3.eth.contract(web3.to_checksum_address(
+            value), abi=contract_abi)
+
+        balance_in_wei = contract.functions.balanceOf(address).call()
+        balance_in_eth = balance_in_wei / 1E18
+        
+        weth_balance[key] = balance_in_eth
+
+    return weth_balance
