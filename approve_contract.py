73f3c931251cb9d75444ab75e3a719afda697a4d
brunodev12 <bruno.carthery12@gmail.com>
2023-12-04 12:12:46 -0300

    feat: improved floor price for erc1155

--- approve_contract.py
+++ approve_contract.py
@@ -2,9 +2,54 @@ from web3 import Web3
 import os
 
 
+def isApprovedForAll(asset_contract, chain='matic'):
+
+    operator_address_dict = {
+        "ethereum": "0x1E0049783F008A0085193E00003D00cd54003c71",
+        "matic": "0x1E0049783F008A0085193E00003D00cd54003c71",
+        "arbitrum": "",
+        "optimism": "",
+    }
+
+    contract_abi = [
+        {
+            "inputs": [{"internalType": "address", "name": "", "type": "address"},
+                       {"internalType": "address", "name": "", "type": "address"}],
+            "name": "isApprovedForAll",
+            "outputs": [{"internalType": "bool", "name": "", "type": "bool"}],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ]
+
+    endpoint_dict = {
+        "ethereum": 'ETHEREUM_RPC',
+        "matic": "POLYGON_RPC",
+    }
+
+    endpoint = os.environ.get(endpoint_dict[chain])
+    web3 = Web3(Web3.HTTPProvider(endpoint))
+
+    operator_address = operator_address_dict[chain]
+    operator_address = web3.to_checksum_address(operator_address)
+
+    account_address = os.environ.get('ADDRESS')
+    account_address = web3.to_checksum_address(account_address)
+    asset_contract = web3.to_checksum_address(asset_contract)
+
+    contract = web3.eth.contract(asset_contract, abi=contract_abi)
+
+    try:
+        is_approved = contract.functions.isApprovedForAll(account_address, operator_address).call()
+    except:
+        is_approved = True
+
+    return is_approved
+
+
 def setApproved(asset_contract, chain='matic'):
 
-    contract_address_dict = {
+    operator_address_dict = {
         "ethereum": "0x1E0049783F008A0085193E00003D00cd54003c71",
         "matic": "0x1E0049783F008A0085193E00003D00cd54003c71",
         "arbitrum": "",
@@ -40,8 +85,8 @@ def setApproved(asset_contract, chain='matic'):
     endpoint = os.environ.get(endpoint_dict[chain])
     web3 = Web3(Web3.HTTPProvider(endpoint))
 
-    contract_address = contract_address_dict[chain]
-    contract_address = web3.to_checksum_address(contract_address)
+    operator_address = operator_address_dict[chain]
+    operator_address = web3.to_checksum_address(operator_address)
 
     account_address = os.environ.get('ADDRESS')
     account_address = web3.to_checksum_address(account_address)
@@ -71,12 +116,12 @@ def setApproved(asset_contract, chain='matic'):
     limit_gas = limit_gas_dict[chain]
     chain_id = chain_id_dict[chain]
 
-    if (gas_price_eth * 102682) < limit_gas:
+    if (gas_price_eth * 69335) < limit_gas:
         if balance_eth > limit_gas:
-            transaction = contract.functions.setApprovalForAll(contract_address, True).build_transaction({
+            transaction = contract.functions.setApprovalForAll(operator_address, True).build_transaction({
                 'from': account_address,
                 'value': 0,
-                'gas': 102682,
+                'gas': 69335,
                 'maxFeePerGas': gas_price_wei,
                 'maxPriorityFeePerGas': gas_price_wei,
                 'nonce': web3.eth.get_transaction_count(account_address),
