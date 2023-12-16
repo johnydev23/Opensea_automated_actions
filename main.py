e893d168a91fc922dab897f311b5b736d1423739
brunodev12 <bruno.carthery12@gmail.com>
2023-12-16 15:20:15 -0300

    feat: migrated api

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import os
 import json
 import csv
-from actions.user_tokens_info import getAssetInfo, getAssetInfo2, getAssetInfo3
-from actions.bidding import getBiddingMessage
-from actions.listing import getListingMessage
-from actions.token_listed_price import getListedPrice
-from actions.token_events import getTokenEvents
+from src.user_tokens_info import getAssetInfo2
+from services.bidding import getBiddingMessage
+from services.listing import getListingMessage
+from utils.get_num_assets import getNumAssets
+from utils.find_asset import findTrait, findAsset
+from utils.check_listing import checkListing
+from utils.concurrent_utils import getBalanceAndUserAssets
 from weth_balance import getWETHbalance
 from database.connection import closeConnection
-import time
 
 address = os.environ.get('ADDRESS')
-balance = getWETHbalance(address)
-collection_list = []
-data_user_contracts, data_user_contracts_info = getAssetInfo2(address, total_items=[])
+
+balance, data_user_contracts, data_user_contracts_info = getBalanceAndUserAssets(getWETHbalance, getAssetInfo2, address)
 
 for filename in os.listdir('.'):
     if filename.endswith('.csv'):
         csv_file = filename
         break
 
+collection_list = []
 with open(csv_file, 'r') as file:
     csv_reader = csv.DictReader(file)
     for row in csv_reader:
@@ -45,136 +46,68 @@ def add_element(element):
         json.dump(collection_info, jsonfile)
 
 for i,v in enumerate(collection_list):
+
     collection_element = collection_list[i]
     slug = v['slug']
     asset_contract = v['contract'].upper()
     number_offers = int(v['number offers']) if v['number offers'] != '' else 1
+    asset = v['assets'] if v['assets'] != '' else None
+    token_standard = v['token standard']
+    limit_price_enabled:bool = v['limit price enabled'] == "yes"
+
     if i>0:
         if slug != collection_list[i-1]['slug']:
             token_id_list = []
-    asset = v['assets'] if v['assets'] != '' else None
-    if data_user_contracts is None:
-        assetInfo = getAssetInfo(address, slug, asset)
-        try:
-            num_assets = len(assetInfo['assets'])
-            chain = assetInfo[0]['asset_contract']['chain_identifier']
-        except KeyError:
-            continue
-    else:
-        if asset_contract in data_user_contracts:
-            num_assets = data_user_contracts_info[asset_contract]['owns_total']
-            chain = data_user_contracts_info[asset_contract]['chain']
-            assetInfo = data_user_contracts_info[asset_contract]
-            if asset is not None:
-                for j in assetInfo['assets']:
-                    token_id = j['tokenId']
-                    if asset == token_id:
-                        num_assets = data_user_contracts_info[asset_contract]['owns_total']
-                        break
-                    else:
-                        num_assets = 0
-        else:
-            num_assets = 0
+
+
+    num_assets, chain, assetInfo = getNumAssets(data_user_contracts, data_user_contracts_info, address, slug, asset, asset_contract)
+    if num_assets == None:
+        continue
+
     found = False
+    check_listing = False
     for z in range(number_offers if number_offers > 0 else 1):
         message = None
         trait = v['trait'] == 'yes'
+        asset_flag = False
+
         if (num_assets > 0) and (num_assets > len(token_id_list)-z):
-            if trait:
-                _type = v['type']
-                _value = v['value']
-                for j in assetInfo['assets']:
-                    if data_user_contracts is None:
-                        traits = j['traits']
-                    else:
-                        traits = j['meta']['attributes']
-                    for k in traits:
-                        key = 'trait_type' if data_user_contracts is None else 'key'
-                        if k[key] == _type and k['value'] == _value:
-                            token_id = j['token_id'] if data_user_contracts is None else j['tokenId']
-                            if token_id not in token_id_list:
-                                last_sale_price = float(j['last_sale']['total_price'])/1E18 if data_user_contracts is None else float(j['lastSale']['price'])
-                                limit_price = last_sale_price
-                                collection_element.update({"limit price": limit_price, "bought": True, "token id": token_id})
-                                token_id_list.append(token_id)
-                                print("List token")
-                                found = True
-                                if data_user_contracts is None:
-                                    listed = j['seaport_sell_orders']
-                                    if listed is None:
-                                        message = getListingMessage(collection_element, address)
-                                    else:
-                                        actual_time = int(time.time())
-                                        offerer = j['seaport_sell_orders'][0]['protocol_data']['parameters']['offerer'].upper()
-                                        expiration_time = int(j['seaport_sell_orders'][0]['expiration_time'])
-                                        if (address.upper() == offerer and expiration_time-actual_time<=300):
-                                            message = getListingMessage(collection_element, address)
-                                else:
-                                    listed = getListedPrice(asset_contract,token_id,chain)
-                                    taker = None
-                                    actual_time = int(time.time())
-                                    expiration_time = actual_time
-                                    try:
-                                        offerer = listed['orders'][0]['protocol_data']['parameters']['offerer'].upper()
-                                        taker = listed['orders'][0]['taker']
-                                        expiration_time = int(listed['orders'][0]['expiration_time'])
-                                    except KeyError:
-                                        offerer = address.upper()
-                                    except IndexError:
-                                        offerer = None
-                                    except TypeError:
-                                        offerer = address.upper()
-                                    if address.upper() != offerer or taker is not None or (address.upper() == offerer and expiration_time-actual_time<=300):
-                                        message = getListingMessage(collection_element, address)
-                                break
-                            if found:
-                                break
-            else:
-                for j in assetInfo['assets']:
-                    token_id = j['token_id'] if data_user_contracts is None else j['tokenId']
-                    if asset is not None:
-                        if asset != token_id:
-                            continue
-                    if token_id not in token_id_list:
-                        if v['token standard']=='ERC-1155':
-                            limit_price = getTokenEvents(chain,asset_contract,asset)
-                            if limit_price is None:
-                                    limit_price = getAssetInfo3(asset_contract, address, asset)
-                        else:
-                            last_sale_price = float(j['last_sale']['total_price'])/1E18 if data_user_contracts is None else float(j['lastSale']['price'])
-                            limit_price = last_sale_price
-                        collection_element.update({"limit price": limit_price, "bought": True, "token id": token_id})
-                        token_id_list.append(token_id)
-                        found = True
-                        print("List token")
-                        if data_user_contracts is None:
-                            listed = j['seaport_sell_orders']        
-                            if listed is None:
-                                message = getListingMessage(collection_element, address)
-                            elif v['token standard']=='ERC-1155':
-                                actual_time = int(time.time())
-                                offerer = j['seaport_sell_orders'][0]['protocol_data']['parameters']['offerer'].upper()
-                                expiration_time = int(j['seaport_sell_orders'][0]['expiration_time'])
-                                if (address.upper() != offerer) or (address.upper() == offerer and expiration_time-actual_time<=300):
-                                    message = getListingMessage(collection_element, address)
-                        else:
-                            listed = getListedPrice(asset_contract,token_id,chain)
-                            taker = None
-                            actual_time = int(time.time())
-                            expiration_time = actual_time
-                            try:
-                                offerer = listed['orders'][0]['protocol_data']['parameters']['offerer'].upper()
-                                taker = listed['orders'][0]['taker']
-                                expiration_time = int(listed['orders'][0]['expiration_time'])
-                            except KeyError:
-                                offerer = address.upper()
-                            except IndexError:
-                                offerer = None
-                            except TypeError:
-                                offerer = address.upper()
-                            if address.upper() != offerer or taker is not None or (address.upper() == offerer and expiration_time-actual_time<=300):
-                                message = getListingMessage(collection_element, address)
-                        break
+            asset_flag = True
+
+        if trait and asset_flag:
+            _type = v['type']
+            _value = v['value']
+            find_trait_results = findTrait(_type, _value, assetInfo, data_user_contracts, token_id_list)
+            if find_trait_results:
+                limit_price, token_id, j = find_trait_results
+                found = True
+                collection_element.update({"limit price": limit_price, "bought": True, "token id": token_id})
+                token_id_list.append(token_id)
+            
+            if found:
+                print("Found Token: ", slug, asset_contract, token_id)
+                check_listing = checkListing(data_user_contracts, j, address, asset_contract, token_id, chain, limit_price_enabled)
+            
+            if check_listing:
+                print("List token")
+                message = getListingMessage(collection_element, address)
+
+        elif asset_flag:
+            find_asset_results = findAsset(assetInfo, data_user_contracts, address, asset, token_id_list, token_standard, chain, asset_contract)
+            if find_asset_results:
+                limit_price, token_id, j = find_asset_results
+                found = True
+                collection_element.update({"limit price": limit_price, "bought": True, "token id": token_id})
+                token_id_list.append(token_id)
+            
+            if found:
+                print("Found Token: ", slug, asset_contract)
+                check_listing = checkListing(data_user_contracts, j, address, asset_contract, token_id, chain, limit_price_enabled)
+            
+            if check_listing:
+                print("List token")
+                message = getListingMessage(collection_element, address)
+            
         if found == False and number_offers>0:
             print("Make offer")
             limit_price, message = getBiddingMessage(v, address, balance, z)
