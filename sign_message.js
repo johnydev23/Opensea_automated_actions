871db2ce6f96a99f992ff8673300616f401a5d25
brunodev12 <bruno.carthery12@gmail.com>
2024-01-25 16:04:56 -0300

    fix: minor fix

--- sign_message.js
+++ sign_message.js
@@ -6,11 +6,16 @@ function signTypedMessage(message) {
     if (message === null){
         return null;
     }
-    const privateKeyOrigin = process.env.PRIVATE_KEY;
-    const privateKey = ethUtil.toBuffer('0x' + privateKeyOrigin);
-    const messageHash = sigUtil.TypedDataUtils.sign(message);
-    const sig = ethUtil.ecsign(messageHash, privateKey);
-    const serializedSig = ethUtil.bufferToHex(sigUtil.concatSig(sig.v, sig.r, sig.s));
+    let serializedSig
+    try {
+        const privateKeyOrigin = process.env.PRIVATE_KEY;
+        const privateKey = ethUtil.toBuffer('0x' + privateKeyOrigin);
+        const messageHash = sigUtil.TypedDataUtils.sign(message);
+        const sig = ethUtil.ecsign(messageHash, privateKey);
+        serializedSig = ethUtil.bufferToHex(sigUtil.concatSig(sig.v, sig.r, sig.s));
+    } catch {
+        serializedSig = null
+    }
     return serializedSig;
 }
 
